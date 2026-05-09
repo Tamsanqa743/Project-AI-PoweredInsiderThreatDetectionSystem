71bee0f8d047333305089f32657a3ab837981224
Tamsanqa743 <Tamsanqathwala62@gmail.com>
2026-05-09 20:47:50 +0200

    move files to App folder

---
+++ App/Controllers/file_controller.py
@@ -0,0 +1,18 @@
+import os
+
+class file_controller:
+
+  def __init__(self):
+    self.uploaded_filename = ''
+
+  
+  def file_upload(self, file, storage_directory):
+    '''save file to device storage'''
+    self.uploaded_filename = file.filename
+    target = os.path.join(storage_directory, file.filename)
+    file.save(target)
+
+
+  def get_filename(self):
+    '''returns name of uploaded file'''
+    return self.uploaded_filename
